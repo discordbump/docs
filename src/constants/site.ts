bbeaffaba008adaff45b8e4ddcb6114c1c67a553
Kyro3400 <jonlichter@bmchsd.org>
2023-10-31 15:19:21 -0400

    Update site.ts

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -2,5 +2,5 @@ export const site: Record<string, any> = {
     'name': 'Vertex Bump',
     'tagline': 'Let the bot do the hard work for you',
     'url': 'https://discordone.tk',
-    'orgName': 'DiscordOne'
+    'orgName': 'discordbump'
 }
